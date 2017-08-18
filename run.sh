5b74f3c059410e4d9627cdc83498bde66071c218
xu.jun <xu.jun@99cloud.net>
2017-08-18 23:24:55 +0800

    ver 0.3.1

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 sudo docker-compose up -d
 # sudo docker-compose exec ftpd pure-pw useradd majik -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u ftpuser -d /home/ftpusers/majik
-sudo docker-compose exec samba chmod 777 /samba
+sudo docker-compose exec samba chown -R 1000:1000 /samba
 
